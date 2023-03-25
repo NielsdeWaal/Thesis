251b4da4836bdc2f14e2cb4023334f23bbb9a106
Niels <nielsdewaal@protonmail.com>
2023-03-25 13:38:51 +0100

    Cleanup

--- DB/source/main.cpp
+++ DB/source/main.cpp
@@ -1,29 +1,26 @@
 // #include <source/EventLoop/EventLoop.h>
+#include "FileManager.hpp"
 #include "InfluxParser.hpp"
 #include "TSC.h"
+
 #include <charconv>
+#include <EventLoop.h>
+#include <File.h>
+#include <fmt/core.h>
 #include <iomanip>
 #include <ranges>
+#include <rigtorp/SPSCQueue.h>
+#include <src/TimeTree.hpp>
 #include <sstream>
 #include <string>
 #include <string_view>
-
-#include <fmt/core.h>
-#include <rigtorp/SPSCQueue.h>
-
-#include <EventLoop.h>
-#include <File.h>
-#include <src/TimeTree.hpp>
-
-#include "FileManager.hpp"
 
 class Storage {
 public:
   virtual ~Storage() = 0;
   virtual void InsertDataBlock(EventLoop::DmaBuffer) = 0;
   virtual EventLoop::DmaBuffer ReadDataBlock(std::uint64_t addr) = 0;
-  virtual void InsertLogStatement(std::uint64_t start, std::uint64_t end,
-                                  std::uint64_t pos) = 0;
+  virtual void InsertLogStatement(std::uint64_t start, std::uint64_t end, std::uint64_t pos) = 0;
 };
 
 // TODO
@@ -37,14 +34,18 @@ public:
 //   Data inserted into memtable corresponding to the tree
 //   When memtable is full, data is flushed to disk and log file
 
-class Handler : public EventLoop::IEventLoopCallbackHandler 
-              //: public Common::IStreamSocketServerHandler
-              //: Common::StreamSocketServer
+class Handler
+: public EventLoop::IEventLoopCallbackHandler
+//: public Common::IStreamSocketServerHandler
+//: Common::StreamSocketServer
 {
 public:
-  explicit Handler(EventLoop::EventLoop &ev)
-      : mEv(ev), mLogFile(mEv), mNodeFile(mEv), mTestFile(mEv),
-        mFileManager(mEv) // , mSocket(mEv, this)
+  explicit Handler(EventLoop::EventLoop& ev)
+  : mEv(ev)
+  , mLogFile(mEv)
+  , mNodeFile(mEv)
+  , mTestFile(mEv)
+  , mFileManager(mEv) // , mSocket(mEv, this)
   {
     mLogger = mEv.RegisterLogger("FrogFishDB");
     mEv.RegisterCallbackHandler(this, EventLoop::EventLoop::LatencyType::Low);
@@ -85,9 +86,9 @@ public:
 
     mStarted = true;
     // auto start = Common::MONOTONIC_CLOCK::Now();
-    mStartTS =  Common::MONOTONIC_CLOCK::Now();
+    mStartTS = Common::MONOTONIC_CLOCK::Now();
 
-    for (auto &measurement : messages) {
+    for (auto& measurement : messages) {
       co_await Writer(measurement);
     }
 
@@ -96,28 +97,19 @@ public:
     // co_return;
   }
 
-  EventLoop::uio::task<> Writer(InfluxMessage &msg) {
-    // while(!mQueue.front()) { co_yield 0; };
-
-    // InfluxMessage* msg = mQueue.front();
-
-    // Return early when there is room in memtable
-    // for()
-    // mLogger->info("Writer received write for {}, ts: {}", msg.name,
-    //               msg.timestamp);
-    for (const auto &measurement : msg.measurments) {
+  EventLoop::uio::task<> Writer(InfluxMessage& msg) {
+    for (const auto& measurement : msg.measurments) {
       std::string name{msg.name + "." + measurement.name};
       if (!mTrees.contains(name)) {
         mLogger->info("New series for {}", name);
         mTrees[name] = MetricTree{};
       }
 
-      auto &db = mTrees[name];
+      auto& db = mTrees[name];
       // FIXME for now we only support longs
       if (std::holds_alternative<std::uint64_t>(measurement.value)) {
         db.memtable[db.ctr] =
-            DataPoint{.timestamp = msg.timestamp,
-                      .value = std::get<std::uint64_t>(measurement.value)};
+            DataPoint{.timestamp = msg.timestamp, .value = std::get<std::uint64_t>(measurement.value)};
         ++db.ctr;
         if (db.ctr == memtableSize) {
           // mLogger->info("Flushing memtable for {}", name);
@@ -125,46 +117,11 @@ public:
           EventLoop::DmaBuffer logBuf = mEv.AllocateDmaBuffer(512);
 
           std::memcpy(buf.GetPtr(), db.memtable.data(), bufSize);
-          // co_await mNodeFile.Append(buf);
-          // auto op =  mNodeFile.Append(buf);
-          // auto op = std::make_pair<EventLoop::SqeAwaitable, EventLoop::deferred_resolver>(mNodeFile.Append(buf), EventLoop::deferred_resolver{});
-          // op.first.SetupData(op.second);
-          // if(!mIOQueue.try_push(op)) {
-          //   while(*mIOQueue.front()->second.result) {  }
-          // } 
-          // mIOQueue.push(op);
-          // if(!mIOQueue.try_emplace(mNodeFile.Append(buf), EventLoop::deferred_resolver{})) {
-          //   while(*mIOQueue.front()->second.result) {  }
-          // }
-          // mIOQueue.push(op);
-          // EventLoop::deferred_resolver resolver;
-          // auto awaitable = mNodeFile.Append(buf);
-          // awaitable.SetupData(resolver);
-          // if(mIOQueue.size() == 32) {
           mIOQueue.push_back(WriteOperation{.buf = std::move(buf), .pos = mFileOffset, .type = File::NODE_FILE});
-            
-          // }
-          // if(!mIOQueue.try_push(std::move(resolver))) {
-          //   while(!(*mIOQueue.front()->result)) {  }
-          //   mIOQueue.push(std::move(resolver));
-          // }
 
-          // if(!mIOQueue.try_push(op)) {
-          //   co_await *mIOQueue.front();
-          //   mIOQueue.pop();
-          //   // op.SetupData();
-          //   // mIOQueue.push(op);
-          // } else {
-            
-          // }
-          // op.SetupData();
-          // op.first.SetupData(op.second);
-          // mIOQueue.push(op);
+          db.tree.Insert(db.memtable.front().timestamp, db.memtable.back().timestamp, mFileOffset);
 
-          db.tree.Insert(db.memtable.front().timestamp,
-                         db.memtable.back().timestamp, mFileOffset);
-
-          LogPoint *log = (LogPoint *)logBuf.GetPtr();
+          LogPoint* log = ( LogPoint* ) logBuf.GetPtr();
           log->start = db.memtable.front().timestamp;
           log->end = db.memtable.back().timestamp;
           log->offset = mFileOffset;
@@ -182,31 +139,34 @@ public:
   }
 
   void OnEventLoopCallback() override {
-    if(mOutstandingIO < 32 && mStarted) {
-      if(mIOQueue.front().type == File::NODE_FILE) {
+    if (mOutstandingIO < 32 && mStarted) {
+      if (mIOQueue.front().type == File::NODE_FILE) {
         mLogger->info("Room to issue request, writing to nodefile at pos: {}", mIOQueue.front().pos);
         EventLoop::SqeAwaitable awaitable = mNodeFile.WriteAt(mIOQueue.front().buf, mIOQueue.front().pos);
-        awaitable.SetCallback([&](int res){IOResolveCallback(res);});
+        awaitable.SetCallback([&](int res) { IOResolveCallback(res); });
       } else if (mIOQueue.front().type == File::LOG_FILE) {
         mLogger->info("Room to issue request, writing to log at pos: {}", mIOQueue.front().pos);
         // mLogFile.WriteAt(mIOQueue.front().buf, mIOQueue.front().pos);
         EventLoop::SqeAwaitable awaitable = mLogFile.WriteAt(mIOQueue.front().buf, mIOQueue.front().pos);
-        awaitable.SetCallback([&](int res){IOResolveCallback(res);});
+        awaitable.SetCallback([&](int res) { IOResolveCallback(res); });
       }
       mIOQueue.pop_front();
       ++mOutstandingIO;
       // issue operation
     }
-    if(mIOQueue.size() == 0 && mStarted) {
+    if (mIOQueue.size() == 0 && mStarted) {
       auto end = Common::MONOTONIC_CLOCK::Now();
-      auto duration =
-          Common::MONOTONIC_CLOCK::ToNanos(end - mStartTS); // / 100 / 100 / 100;
+      auto duration = Common::MONOTONIC_CLOCK::ToNanos(end - mStartTS); // / 100 / 100 / 100;
 
       double timeTakenS = duration / 1000000000.;
       double rateS = mIngestionCounter / timeTakenS;
       double dataRate = (rateS * sizeof(DataPoint)) / 1000000;
-      mLogger->info("Ingestion took, {}ns, {} points, {} points/sec, {} MB/s",
-                    duration, mIngestionCounter, rateS, dataRate);
+      mLogger->info(
+          "Ingestion took, {}ns, {} points, {} points/sec, {} MB/s",
+          duration,
+          mIngestionCounter,
+          rateS,
+          dataRate);
       mEv.Stop();
     }
   }
@@ -250,7 +210,7 @@ private:
     std::uint64_t timestamp;
     std::uint64_t value;
   };
-  EventLoop::EventLoop &mEv;
+  EventLoop::EventLoop& mEv;
   DmaFile mLogFile;
   // AppendOnlyFile mNodeFile;
   DmaFile mNodeFile;
