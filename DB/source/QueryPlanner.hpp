f5a9aa31e0a147f2ce9a93d2dce442d031721080
Niels <nielsdewaal@protonmail.com>
2023-03-30 22:03:41 +0200

    Initial commit of query planner

---
+++ DB/source/QueryPlanner.hpp
@@ -0,0 +1,7 @@
+#ifndef __QUERY_PLANNER
+#define __QUERY_PLANNER
+
+// Influx query: 
+// SELECT max(usage_user) from cpu where (hostname = 'host_1') and time >= '2016-01-01T18%3A24%3A41Z' and time < '2016-01-01T19%3A24%3A41Z' group by time(1m)
+
+#endif // __QUERY_PLANNER
