941fcd3b706a8636b62ca50f421acf3a5adab4eb
Niels <nielsdewaal@protonmail.com>
2023-03-30 22:28:26 +0200

    WIP Querying

--- DB/source/QueryPlanner.hpp
+++ DB/source/QueryPlanner.hpp
@@ -1,7 +1,27 @@
 #ifndef __QUERY_PLANNER
 #define __QUERY_PLANNER
 
+#include <lexy/callback.hpp>
+#include <lexy/dsl.hpp>
+#include <string>
+
+// https://forcedotcom.github.io/phoenix/
 // Influx query: 
-// SELECT max(usage_user) from cpu where (hostname = 'host_1') and time >= '2016-01-01T18%3A24%3A41Z' and time < '2016-01-01T19%3A24%3A41Z' group by time(1m)
+// SELECT max(usage_user) from cpu where (hostname = 'host_1') and time >= '2016-01-01T18-24-41Z' and time < '2016-01-01T19-24-41Z' group by time(1m)
+
+namespace grammar {
+  namespace dsl = lexy::dsl;
+
+  struct table {
+        static constexpr auto rule = dsl::identifier(dsl::ascii::alpha_digit_underscore);
+        static constexpr auto value = lexy::as_string<std::string>;
+    };
+
+    struct expression {
+        static constexpr auto rule = LEXY_LIT("select");
+    };
+
+    static constexpr auto rule = dsl::p<expression> + dsl::lit_c<' '> + dsl::p<table>;
+}
 
 #endif // __QUERY_PLANNER
