8b3595d64b1dbce1237354229f849d25639bffb4
Niels <nielsdewaal@protonmail.com>
2023-03-31 11:15:20 +0200

    WIP query parsing

--- DB/source/QueryPlanner.hpp
+++ DB/source/QueryPlanner.hpp
@@ -6,22 +6,51 @@
 #include <string>
 
 // https://forcedotcom.github.io/phoenix/
-// Influx query: 
-// SELECT max(usage_user) from cpu where (hostname = 'host_1') and time >= '2016-01-01T18-24-41Z' and time < '2016-01-01T19-24-41Z' group by time(1m)
+// https://godbolt.org/z/vYqaM1xah
+// Influx query:
+// SELECT max(usage_user) from cpu where (hostname = 'host_1') and time >= '2016-01-01T18-24-41Z' and time <
+// '2016-01-01T19-24-41Z' group by time(1m)
 
 namespace grammar {
   namespace dsl = lexy::dsl;
 
   struct table {
-        static constexpr auto rule = dsl::identifier(dsl::ascii::alpha_digit_underscore);
-        static constexpr auto value = lexy::as_string<std::string>;
-    };
+    static constexpr auto rule = dsl::identifier(dsl::ascii::alpha_digit_underscore) | dsl::lit_c<'*'>;
+    static constexpr auto value = lexy::as_string<std::string>;
+  };
 
-    struct expression {
-        static constexpr auto rule = LEXY_LIT("select");
-    };
+  struct select_expression {
+    static constexpr auto rule = LEXY_LIT("select") + dsl::lit_c<' '> + dsl::p<table>;
+    static constexpr auto value = lexy::as_string<std::string>;
+  };
 
-    static constexpr auto rule = dsl::p<expression> + dsl::lit_c<' '> + dsl::p<table>;
-}
+  struct table_expression {
+    static constexpr auto rule = LEXY_LIT("from") + dsl::lit_c<' '> + dsl::p<table>;
+  };
+
+  struct where_expression {
+    static constexpr auto rule = LEXY_LIT("where");
+  };
+
+  struct compare: dsl::infix_op_left {
+    static constexpr auto op = dsl::op(dsl::lit_c<'*'>) / dsl::op(dsl::lit_c<'/'>);
+    using operand = dsl::atom;
+  };
+
+  struct condition {
+    // static constexpr auto rule =
+  };
+
+  struct and_condition {};
+
+  struct or_condition {};
+
+  struct select {
+    static constexpr auto rule = dsl::p<select_expression> + dsl::lit_c<' '> + dsl::p<table_expression>
+                                 + dsl::opt(dsl::lit_c<' '> >> dsl::p<where_expression>);
+  };
+
+  static constexpr auto rule = dsl::p<select>;
+}; // namespace grammar
 
 #endif // __QUERY_PLANNER
