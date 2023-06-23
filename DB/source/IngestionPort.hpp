0ff9f17c6b0d6e537eba1d22717e38bdbbb657e4
Niels <nielsdewaal@protonmail.com>
2023-06-23 17:10:06 +0200

    Change ingestion port to use websockets

--- DB/source/IngestionPort.hpp
+++ DB/source/IngestionPort.hpp
@@ -2,28 +2,48 @@
 #define __INGESTION_PORT
 
 #include "IngestionProtocol/proto.capnp.h"
+#include "StreamSocket.h"
 #include "TSC.h"
 #include "UDPSocket.h"
+#include "WebSocketServer.hpp"
 #include "Writer.hpp"
 
 #include <capnp/common.h>
 #include <capnp/serialize.h>
 
-template<std::size_t bufSize> class IngestionPort: public Common::IUDPSocketHandler {
+template<std::size_t bufSize>
+class IngestionPort
+// : public Common::IStreamSocketServerHandler
+// , public Common::IStreamSocketHandler
+: public FrogFish::IWebSocketServerHandler
+, public FrogFish::IWebSocketHandler {
 public:
-  IngestionPort(EventLoop::EventLoop& ev, Writer<bufSize>& writer): mSocket(ev, this), mWriter(writer) {
+  IngestionPort(EventLoop::EventLoop& ev, Writer<bufSize>& writer): /*mSocket(ev, this),*/ mWriter(writer), mWebSocket(ev, this) {
     mLogger = ev.RegisterLogger("IngestionPort");
   }
 
   void Configure(std::uint16_t port) {
-    mSocket.StartListening(nullptr, port);
+    // mSocket.StartListening(nullptr, port);
+    // mSocket.BindAndListen(port);
+    mWebSocket.Configure(port);
   }
 
-  void OnIncomingData([[maybe_unused]] char* data, [[maybe_unused]] size_t len) override {
+  void OnConnected() final {}
+  // void OnDisconnect([[maybe_unused]] Common::StreamSocket* conn) final {}
+  void OnDisconnect([[maybe_unused]] websocketpp::connection_hdl conn) final {}
+
+  // Common::IStreamSocketHandler* OnIncomingConnection() {
+  FrogFish::IWebSocketHandler* OnIncomingConnection() {
+    mLogger->info("New incoming client");
+    return static_cast<FrogFish::IWebSocketHandler*>(this);
+  }
+
+  // void OnIncomingData(Common::StreamSocket* conn, char* data, size_t len) override {
+  void OnIncomingData([[maybe_unused]] websocketpp::connection_hdl conn, FrogFish::ServerSocket::message_ptr data) final {
     // mLogger->info("Received udp frame: {}", std::string{data, len});
-    // auto buf = std::aligned_alloc(512, len);
-    // std::memcpy(buf, data, len);
-    auto pdata = kj::arrayPtr(( const capnp::word* ) data, len / sizeof(capnp::word));
+    auto buf = std::aligned_alloc(512, data->get_payload().size());
+    std::memcpy(buf, data->get_payload().data(), data->get_payload().size());
+    auto pdata = kj::arrayPtr(( const capnp::word* ) buf, data->get_payload().size() / sizeof(capnp::word));
     capnp::FlatArrayMessageReader msg{pdata};
     proto::InsertionBatch::Reader insertMsg = msg.getRoot<proto::InsertionBatch>();
 
@@ -42,13 +62,33 @@ public:
     double timeTakenS = duration / 1000000000.;
     double rateS = ingestCount / timeTakenS;
     double dataRate = (rateS * 128) / 1000000;
-    // mLogger->info("Ingested {} points in {}s, rate: {}MB/s / {} points/sec", ingestCount, timeTakenS, dataRate,
-    // rateS);
+    mLogger->info(
+        "Ingested {} ({} bytes) points in {}s, rate: {}MB/s / {} points/sec",
+        ingestCount,
+        data->get_payload().size(),
+        timeTakenS,
+        dataRate,
+        rateS);
+
+    ::capnp::MallocMessageBuilder response;
+    proto::InsertionResponse::Builder resp = response.initRoot<proto::InsertionResponse>();
+
+    auto encodedArray = capnp::messageToFlatArray(response);
+    auto encodedArrayPtr = encodedArray.asChars();
+    auto encodedCharArray = encodedArrayPtr.begin();
+    auto size = encodedArrayPtr.size();
+
+    // conn->Send(encodedCharArray, size);
+    mWebSocket.Send(conn, encodedCharArray, size);
   }
 
 private:
-  Common::UDPSocket mSocket;
+  // Common::UDPSocket mSocket;
+  // Common::StreamSocketServer mSocket;
   Writer<bufSize>& mWriter;
+
+  FrogFish::WebSocket mWebSocket;
+
   std::shared_ptr<spdlog::logger> mLogger;
 };
 
